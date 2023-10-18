9812cb29f3978ab6937d4ce81a09863ccddb44e3
Igor Kuznecov <is@kuznecov8.ru>
2023-10-18 22:05:35 +0300

    for review2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,22 +1,26 @@
-const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/unauthorized-err');
+const jwt = require("jsonwebtoken");
+const { NODE_ENV, JWT_SECRET } = process.env;
+const UnauthorizedError = require("../errors/unauthorized-err");
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
+const extractBearerToken = (header) => header.replace("Bearer ", "");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError("Необходима авторизация");
   }
 
   const token = extractBearerToken(authorization);
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "super-strong-secret"
+    );
   } catch (error) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError("Необходима авторизация"));
   }
 
   req.user = payload;
